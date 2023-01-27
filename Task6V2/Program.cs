542f28d33373e844e80dbbc9b84ffe12e780af54
thenewlife <thenewlife@mail.ru>
2023-01-27 18:03:41 +0300

    Added Home Tasks

---
+++ Task6V2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+if(number%2==0)
+{
+    Console.WriteLine("Это четное число");
+}
+else
+{
+     Console.WriteLine("Это нечетное число");
+}
